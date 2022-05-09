8f768d41e741cb78b3c76043de61c9669d6e6405
EmanuelMTDF <104039755+EmanuelMTDF@users.noreply.github.com>
2022-05-09 20:17:29 -0300

    Actualizacion de codigo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 //EJERCICIO "SUMA ENTRE NUMEROS"
 
-let numero1: number = Number(prompt("Ingrese el primer número: "));
-let numero2: number = Number(prompt("Ingrese el segundo número"));
+let numero1: number = Number(prompt("Por favor, ingrese el primer número: "));
+let numero2: number = Number(prompt("Por favor, ingrese el segundo número:"));
 let resultado: number = 0;
 
 for (numero1; numero1 <= numero2; numero1++) {
