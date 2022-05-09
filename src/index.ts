82c3d79cd58a476c1827e2781448fc42f1b32eb2
EmanuelMTDF <104039755+EmanuelMTDF@users.noreply.github.com>
2022-05-09 20:16:17 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+//EJERCICIO "SUMA ENTRE NUMEROS"
+
+let numero1: number = Number(prompt("Ingrese el primer número: "));
+let numero2: number = Number(prompt("Ingrese el segundo número"));
+let resultado: number = 0;
+
+for (numero1; numero1 <= numero2; numero1++) {
+  resultado = resultado + numero1;
+}
+console.log("El resultado es: ", resultado);
