ad3b5ffa58fe0392bd88526f86693ce48e282813
EmanuelMTDF <104039755+EmanuelMTDF@users.noreply.github.com>
2022-05-16 00:01:06 -0300

    Actualizacion de datos

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 //EJERCICIO "SUMA ENTRE NUMEROS"
 
+/*
+• Escriba un programa que pida al usuario dos números
+enteros, y luego retorne la suma de todos los números
+que están entre ellos
+• Por ejemplo, si los números son 2 y 7, debe entregar
+como resultado 2 + 3 + 4 + 5 + 6 + 7 = 27
+*/
+
 let numero1: number = Number(prompt("Por favor, ingrese el primer número: "));
 let numero2: number = Number(prompt("Por favor, ingrese el segundo número:"));
 let resultado: number = 0;
